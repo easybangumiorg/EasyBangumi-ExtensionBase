7a8a745e469043cdecd944d0756fe92fbc02a058
heyanLe <1371735400@qq.com>
2023-06-05 16:14:11 +0800

    first

---
+++ extension-app/src/main/java/com/heyanle/easybangumi_extension/EasySourceFactory.kt
@@ -0,0 +1,18 @@
+package com.heyanle.easybangumi_extension
+
+import com.heyanle.bangumi_source_api.api.Source
+import com.heyanle.bangumi_source_api.api.SourceFactory
+import com.heyanle.easybangumi_extension.yhdm.YhdmSource
+
+/**
+ * Created by HeYanLe on 2023/2/19 23:23.
+ * https://github.com/heyanLE
+ */
+class EasySourceFactory: SourceFactory {
+
+    override fun create(): List<Source> {
+        return listOf(
+            // 添加你的源
+        )
+    }
+}
